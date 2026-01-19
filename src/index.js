f63ac53b7d8ef9b614afdc4663d0ca155f7bf317
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2026-01-19 16:06:12 -0500

    Remove text after | or : in hymn names

--- src/index.js
+++ src/index.js
@@ -31,7 +31,13 @@ function exportToFourscore() {
 
   const propers = rows(table)
     .filter(row => isProper(row.getCell(0).getText()))
-    .map(row => row.getCell(1).getText());
+    .map(row => row.getCell(1).getText())
+    // Some names look like
+    // Here At This Table | Anthology 8.2
+    // Psalm 40: Here am I, Lord; I come to do your will.
+    // Take just the first part.
+    .map(name => name.split("|")[0].trim())
+    .map(name => name.split(":")[0].trim());
 
   const contents = forScoreSetlist([title, ...propers, "Mass Parts"]);
 
